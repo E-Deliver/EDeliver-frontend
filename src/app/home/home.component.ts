2100235f9248009e0635f6321dc52a20a4f53083
Khadija El Madani <khadijaelmadani24@gmail.com>
2024-10-17 16:48:27 +0000

    Add: Landing and home pages missing some styles

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css'],
+  encapsulation: ViewEncapsulation.None
+})
+export class HomeComponent implements OnInit{
+
+  constructor(private router: Router) { }
+
+  ngOnInit() { }
+
+  navigateToHome() {
+    this.router.navigate(['/']);
+  }
+
+  navigateToLogin() {
+    this.router.navigate(['/login']);
+  }
+
+  navigateToSignup() {
+    this.router.navigate(['/signup']);
+  }
+}
