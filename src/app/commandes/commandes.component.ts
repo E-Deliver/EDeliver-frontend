de2ba51f1662c1a09e2a760bdca016b656f25308
Khadija El Madani <khadijaelmadani24@gmail.com>
2024-10-24 00:24:40 +0100

    Fix: some frontend adjustments for commands section

--- src/app/commandes/commandes.component.ts
+++ src/app/commandes/commandes.component.ts
@@ -1,4 +1,6 @@
 import { Component, Inject, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/services/auth.service';
 import { CommandeService } from 'src/services/commande.service';
 @Component({
   selector: 'app-commandes',
@@ -9,11 +11,14 @@ import { CommandeService } from 'src/services/commande.service';
 
 export class CommandesComponent implements OnInit {
 
+  user: any;
+
   commandes: any[] = [];  // Store the list of commandes
 
-  constructor(@Inject(CommandeService) private commandeService: CommandeService) { }
+  constructor(@Inject(CommandeService) private commandeService: CommandeService, private authService: AuthService, private router: Router) { }
 
   ngOnInit(): void {
+    this.user = this.authService.getUserData(); 
     this.fetchCommandes();  // Fetch commandes when the component is initialized
   }
 
@@ -34,5 +39,10 @@ export class CommandesComponent implements OnInit {
     // Logique pour ouvrir un modal ou assigner un livreur
     console.log("Choisir un livreur pour la commande : ", commande.idCommande);
     // Ici, vous pouvez ajouter la logique pour ouvrir un modal et sélectionner un livreur.
+  }
+
+  logout(): void {
+    localStorage.clear(); 
+    this.router.navigate(['/']); 
   }
 }
